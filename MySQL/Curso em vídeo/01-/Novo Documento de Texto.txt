296f0bdc049f845425552144cee1f3d96b3565e3
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-21 20:21:08 -0300

    criando chave primária

--- MySQL/Curso em vídeo/01-/Novo Documento de Texto.txt
+++ MySQL/Curso em vídeo/01-/Novo Documento de Texto.txt
@@ -1,17 +1,18 @@
 create database pacientes;		#cria um banco de dados.
+#drop database pacientes;		#apaga o banco de dados
 
 show databases;					#mostra os bancos de dados existentes
 
 use pacientes;				#Abre o banco de dados
 
 create table pessoa(			#cria uma tabela
-	#criando os CAMPOS e seus TIPOS( numérico, data/tempo, literal e espacial).
+	#criando os CAMPOS (registros são criados com campos) e seus TIPOS( numérico, data/tempo, literal e espacial).
     #NUMÉRICOS: Inteiro (TinyInt, SmallInt, int, MediumInt, BigInt), Real(Decimal, Double, Float, Real) , Lógico(Bit, Boolean)
     #DATA/TEMPO: Date, DateTime, TimeStamp, Time, Year
     #LITERAIS: Caractere (Char(fixo), VarChar(variável)), Texto (TinyText, Text, MediumText, LongText), Binário (TinyBlob, Blob, MediumBlob, LongBlob), coleção(Enum, Set) 
     #ESPACIAL: Geometry, Point, Polygon, MultiPolygon
     nome varchar(30),
-    idade tinyint(3),
+    idade tinyint,	#ocupa três bits
     peso float,
     altura float,
     sexo char(1),
---
+++ MySQL/Curso em vídeo/02-/02.sql
@@ -0,0 +1,17 @@
+create database cadastro		#cria um banco de dados com constraitns.
+default character set utf8		#codificação de caracteres (utf8 possui acentos)
+default collate utf8_general_ci;	# definindo um collation
+
+use cadastro;				#Abre o banco de dados
+
+create table pessoa(			#cria uma tabela
+	id int not null auto_increment,			#cria um identificador para cada pessoa cadastradas. (int tem 11 bits). (auto_increment incrementa automaticamente)
+    nome varchar(30) not null,	#not null obriga a preencher a variável
+    nascimento date,	#guarda a data de nascimento para calcular a idade
+	sexo enum('M', 'F'),	#aceita paenas M e F
+    peso decimal(5, 2),	#cria um vetor numérico com cinco valores, dois usados após a vírgula
+    altura  decimal(3, 2),   
+    nascionalidade varchar(20) default 'Brasil',  	#cria uma string, caso não preenchida, recebe o valor 'brasil'  
+    primary key(id)				#transforma o campo 'id' em chave primária. Não permite cadastrar o mesmo id
+    
+)default charset = utf8;
