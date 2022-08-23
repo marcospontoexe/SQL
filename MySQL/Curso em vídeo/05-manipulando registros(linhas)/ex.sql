cab7f86ba2225be9642f23f5daa4ecdcec06b8d1
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-22 21:55:40 -0300

    manipulando linhas

---
+++ MySQL/Curso em vídeo/05-manipulando registros(linhas)/ex.sql
@@ -0,0 +1,8 @@
+select * from cursos;
+
+insert into cursos values								#inserindo registros (linhas) na tabela
+('1', 'PJP', 'Curso de php', '40', '40', default),
+('2', 'HTML', 'Curso de html', '30', '15', '2021'),
+('3', 'JavaEScripte', 'curso', '40', '10', '2020'),
+('4', 'Python', 'tutorial', '10', '10', '2000');
+
